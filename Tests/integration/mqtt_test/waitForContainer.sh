e2aa595bc1538080bfc715e76b12be64c8400136
Sebastian Friedl <sebastian.friedl@me.com>
2025-02-10 09:46:48 +0100

    fix waitForContainer

--- Tests/integration/mqtt_test/waitForContainer.sh
+++ Tests/integration/mqtt_test/waitForContainer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 NEXT_WAITTIME=0
 WAITTIME_LIMIT_SEC=600
-while [[ "$(docker logs  mqtt_test-gateway-1 | grep -c "Publish Bad List Maschine Nodes finish.")" > "2"  && "$NEXT_WAITTIME" != "$WAITTIME_LIMIT_SEC" ]]
+echo "Waiting for test container to become ready since ${NEXT_WAITTIME}s..."
+while [[ "$(docker logs  mqtt_test-gateway-1 | grep -c "Publish Bad List Maschine Nodes finish.")" -le "2"  && "$NEXT_WAITTIME" != "$WAITTIME_LIMIT_SEC" ]]
 do 
     echo "Waiting for test container to become ready since ${NEXT_WAITTIME}s..."
     sleep 5
